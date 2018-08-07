acee20ef5a7dfc7d5e31f74531c83e067fbb5df3
shigeki uchida <shigeki.uchida@gmail.com>
2018-08-07 15:52:23 +0900

    メッセージ終了
    ただし、menber表示を除く

--- app/models/group.rb
+++ app/models/group.rb
@@ -6,8 +6,8 @@ class Group < ApplicationRecord
   validates :name, presence: true
 
   def show_last_message
-    if (last_message = message.last).present?
-      last_message.content? ? last_message.content : '画像が島弧されています。'
+    if (last_message = messages.last).present?
+      last_message.content? ? last_message.content : '画像が投稿されています。'
     else
       'まだメッセージはありません。'
     end
