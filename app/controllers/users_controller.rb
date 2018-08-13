bd561cd9e4e6f425e2e3e0ab31b5fff444f0cb2a
内田茂樹 <shigeki.uchida@gmail.com>
2018-08-13 16:15:47 +0900

    users_controller微調整

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,8 @@
 class UsersController < ApplicationController
 
   def index
-    @users = User.where('title LIKE(?)', "%#{params[:keyword]}%")
+    binding.pry
+    @users = User.where('name LIKE(?)', "%#{params[:keyword]}%")
     respond_to do |format|
       format.html
       format.json
