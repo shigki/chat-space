e0b5d21985737f3c3fd24116a18951fdb5311f03
内田茂樹 <shigeki.uchida@gmail.com>
2018-08-14 12:48:53 +0900

    Edit search.js
    trim

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -7,31 +7,37 @@ function appendUser(user) {
 								<p class="chat-group-user-name">
 									${user.name}
 								</p>
-
+								<div class="chat-group-user__btn">
+									<a class="chat-group-user__btn--add">
+										追加
+									</a>
+								</div>
 							</div>`
   user_list.append(html);
 }
 
 function appendNoUser(message) {
-	var html = `<p class="chat-group-user-name">
-									= message
-								</p>`
+	var html = `<div class="chat-group-user">
+								<p class="chat-group-user-message">
+									${message}
+								</p>
+							</div>`
 	user_list.append(html);
 }
 
   $('#user-search-field').on("keyup", function() {
   	var input = $(this).val();
+  	var trimmed_input = input.trim();
 
   	$.ajax({
   		type: 'GET',
   		url: '/users',
-  		data: { keyword: input },
+  		data: { keyword: trimmed_input },
   		dataType: 'json'
   	})
   	.done(function(users) {
   		$("#user-search-result").empty();
   		if (users.length !==0) {
-  			console.log(users);
   			users.forEach(function(user){
   				appendUser(user);
   			});
