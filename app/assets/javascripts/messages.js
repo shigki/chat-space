653ecd9339f5cf7badd004f794a2775b5659c0cc
内田茂樹 <shigeki.uchida@gmail.com>
2018-08-16 10:45:28 +0900

    Edit Messages.js
    ajaxの発火

--- app/assets/javascripts/messages.js
+++ app/assets/javascripts/messages.js
@@ -37,4 +37,16 @@ $(function(){
     })
     return false;
   })
+
+  setInterval(function(){
+    $.ajax({
+      url: location.href.json,
+    })
+    .done(function(data){
+    })
+    .fail(function(data){
+    });
+  } else {
+    clearInterval(interval);
+  } ,5000 );
 });
